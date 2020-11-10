07bd9eff656d963e1ab3425158827d9181079b71
Gianazza <roxana.gianazza1@bhp.com>
2020-11-10 20:19:20 +0800

    removed permissions to create organisations

--- groupProjectBackend/organisations/views.py
+++ groupProjectBackend/organisations/views.py
@@ -23,7 +23,7 @@ class OrganisationList(ListAPIView):
     def post(self, request):
         serializer = OrganisationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
